0d268ad2a40b8fa8c195b47eb00152805b82d708
laurennrmoore <lmooreintech2021@gmail.com>
2021-04-17 18:42:55 -0400

    arrowhwpush

---
+++ arrowHw/wk3day5app.js
@@ -0,0 +1,42 @@
+//Creating Variables with numeric values
+//var a = 20
+//var b = 10
+
+
+function addTwoNumbers(a,b){
+ return a+b;   
+}
+
+function subtractTwoNumbers(a,b){
+ return a-b;   
+}
+
+
+function multiplyTwoNumbers (a,b){
+ return a*b;
+}
+
+
+function divideTwoNumbers(a,b){
+ return a/b;   
+}
+
+//multiple arithmetic operators featuring the arrow function + 
+//'const' keyword 
+
+const add = (a,b) => {
+ return a+b;
+}
+  
+const subtract = (a,b) => {
+ return a-b; 
+}  
+
+const multiply = (a,b) => {
+ return a*b;
+}
+
+const divide = (a,b) => {
+ return a/b;
+}
+
